877e40a0c76ea57570470d19fed9067575a64f70
louiseleighhh <louisecapala@gmail.com>
2022-07-06 22:54:49 +0900

    was able to add create review form on restaurant show, need to design though

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,13 @@
 class RestaurantsController < ApplicationController
   # index, show, new, create
-  before_action :find_task, only: %i[show edit update]
+  before_action :find_restaurant, only: %i[show edit update]
   def index
     @restaurants = Restaurant.all
   end
 
-  def show; end
+  def show
+    @review = Review.new
+  end
 
   def new
     @restaurant = Restaurant.new
@@ -13,7 +15,20 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to task_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,9 @@
 class ReviewsController < ApplicationController
-  before_action :find_restaurant, except: [:destroy]
+  before_action :find_restaurant
 
-  def new
-    @review = Revie.new
-  end
+  # def new
+  #   @review = Review.new
+  # end
 
   def create
     @review = Review.new(review_params)
@@ -11,17 +11,17 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
   private
 
   def find_restaurant
-    @restaurant = Restaurant.find(params[restaurant_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
